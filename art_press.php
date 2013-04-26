23e3fafd794111a33c2b8045bc6d37920717bbb1
Niels van Hecke <niels.van.hecke@gmail.com>
2013-04-25 18:06:52 -0600

    Fixed data input and storage for art
    The art input dialog looks and acts better now.
    Metadata is only stored for images that have metadata to store.

--- art_press.php
+++ art_press.php
@@ -37,13 +37,14 @@ function attachment_art_info( $form_fields, $post ) {
 		'input' => 'html',
 		'html' => "<label for='is_art'>Art item for sale? </label><input name='attachments[{$post->ID}][is_art]' type='checkbox' id='is_art' ".($is_art?"checked='true'":"")."/>
                             <div id='art_info' style='display: none;'>
+                            <label for='attachments[{$post->ID}][art_type]'>Art Category:</label>
                                 <select name='attachments[{$post->ID}][art_type]' id='attachments[{$post->ID}][art_type]'>
                                   <option value='paint'".($current_type == 'paint'?' selected':'').">Paint</option>
                                   <option value='sculpture'".($current_type == 'sculpture'?' selected':'').">Sculpture</option>
                                   <option value='mixed'".($current_type == 'mixed'?' selected':'').">Mixed Media</option>
                                   <option value='photo'".($current_type == 'photo'?' selected':'').">Photography</option>
-                                </select>
-                                <input name='attachments[{$post->ID}][art_price]' type='text' id='art_price' value='".($price?$price:"0")."' />
+                                </select><br />
+                                <label for='art_price'>Art Price:</label><input name='attachments[{$post->ID}][art_price]' type='text' id='art_price' value='".($price?$price:"0")."' />
                             </div>",
 		'helps' => 'Specify the art medium or type.',
 	);
@@ -54,33 +55,31 @@ function attachment_art_info( $form_fields, $post ) {
 add_filter( 'attachment_fields_to_edit', 'attachment_art_info', 10, 2 );
 
 function attachment_art_info_save( $post, $attachment ) {
-    $post_ob = get_post($post->ID);
-    // check to see if there is a button already
-    $button = get_post_meta($post->ID, 'PPButton');
+    if($attachment['is_art']){
+        $post_ob = get_post($post->ID);
+        // check to see if there is a button already
+        $button = get_post_meta($post->ID, 'PPButton');
 
-    $buttonSearchRequest = new BMButtonSearchRequestType();
-    $buttonSearchRequest->StartDate = $post_ob->post_date;
 
-    $buttonSearchReq = new BMButtonSearchReq();
-    $buttonSearchReq->BMButtonSearchRequest = $buttonSearchRequest;
 
-    $paypalService = new PayPalAPIInterfaceServiceService();
-    try {
-        $buttonSearchResponse = $paypalService->BMButtonSearch($buttonSearchReq);
-        error_log(print_r($buttonSearchResponse, true));
-    } catch (Exception $ex) {
-        require '../Error.php';
+        $paypalService = new PayPalAPIInterfaceServiceService();
+        try {
+            $buttonSearchResponse = $paypalService->BMButtonSearch($buttonSearchReq);
+            error_log(print_r($buttonSearchResponse, true));
+        } catch (Exception $ex) {
+            require '../Error.php';
+        }
+        // update old button
+        if($button){}
+        // create new button
+        else{}
+        // set new button
+
+
+        update_post_meta($post['ID'], 'is_art', $attachment['is_art']);
+        update_post_meta( $post['ID'], 'art_type', $attachment['art_type'] );
+        update_post_meta( $post['ID'], 'art_price', $attachment['art_price'] );
     }
-    // update old button
-    if($button){}
-    // create new button
-    else{}
-    // set new button
-
-    
-    update_post_meta($post['ID'], 'is_art', $attachment['is_art']);
-    update_post_meta( $post['ID'], 'art_type', $attachment['art_type'] );
-    update_post_meta( $post['ID'], 'art_price', $attachment['art_price'] );
     return $post;
 }
 
